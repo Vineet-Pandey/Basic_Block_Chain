d0d79b860c2d82f8d729ca208c5e57f29eac98aa
vineetpandey <vineetpandey1729@gmail.com>
2024-12-06 14:03:24 -0500

    documentation added for utility

--- include/utility.h
+++ include/utility.h
@@ -1,7 +1,7 @@
-//
-// Created by vineet on 12/6/24.
-//
-
+/// @file utility.h
+/// @brief Utility functions for serialization.
+/// @author Vineet
+/// @date 12/6/24
 #ifndef UTILITY_H
 #define UTILITY_H
 #include <string>
@@ -9,21 +9,49 @@
 namespace IronBlock {
 
 namespace Utility {
+/**
+ * @brief Serializes data of type T to a string.
+ *
+ * @tparam T The type of data to serialize.
+ * @param data The data to serialize.
+ * @return A string representation of the data.
+ */
 template <typename T> std::string serialize(const T &data);
 
-// for integers
+/**
+ * @brief Specialization for serializing integers to a string.
+ *
+ * @param data The integer to serialize.
+ * @return A string representation of the integer.
+ */
 template <> std::string serialize(const int &data) {
   return std::to_string(data);
 }
-// for doubles
+/**
+ * @brief Specialization for serializing doubles to a string.
+ *
+ * @param data The double to serialize.
+ * @return A string representation of the double.
+ */
 template <> std::string serialize(const double &data) {
   return std::to_string(data);
 }
 
-// for strings
+/**
+ * @brief Specialization for serializing strings.
+ *
+ * @param data The string to serialize.
+ * @return The original string.
+ */
 template <> std::string serialize(const std::string &data) { return data; }
 
-// for vectors
+/**
+ * @brief Serializes a vector of data to a string.
+ *
+ * @tparam T The type of elements in the vector.
+ * @param data The vector to serialize.
+ * @return A string representation of the vector.
+ */
 template <typename T> std::string serialize(const std::vector<T> &data) {
   std::stringstream ss;
   for (size_t i = 0; i < data.size(); i++) {
@@ -32,7 +60,14 @@ template <typename T> std::string serialize(const std::vector<T> &data) {
   return ss.str();
 }
 
-// for maps
+/**
+ * @brief Serializes a map of data to a string.
+ *
+ * @tparam T The type of keys in the map.
+ * @tparam K The type of values in the map.
+ * @param data The map to serialize.
+ * @return A string representation of the map.
+ */
 template <typename T, typename K>
 std::string serialize(const std::map<T, K> &data) {
   std::stringstream ss;
@@ -41,7 +76,7 @@ std::string serialize(const std::map<T, K> &data) {
     ss << serialize(it->first);
     ss << ":";
     ss << serialize(it->second);
-    ss<<" ";
+    ss << " ";
   }
   return ss.str();
 }
