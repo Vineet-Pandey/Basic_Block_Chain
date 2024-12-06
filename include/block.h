d9d585f2a8eab470411dfeb59d06dffb0bf6aad5
vineetpandey <vineetpandey1729@gmail.com>
2024-12-06 10:26:42 -0500

    create block class

--- include/block.h
+++ include/block.h
@@ -4,5 +4,47 @@
 
 #ifndef BLOCK_H
 #define BLOCK_H
+#include "utility.h"
+#include <ctime>
+#include <sodium.h>
+#include <sstream>
+namespace IronBlock {
+class Block {
+public:
+  int index;
+  std::string data;
+  std::string previous_block_hash;
+  std::time_t timestamp;
+  std::string block_hash;
 
-#endif //BLOCK_H
+  Block(int index, std::string &data, std::string &previous_block_hash)
+      : index(index), data(data), previous_block_hash(previous_block_hash) {
+    timestamp = std::time(nullptr);
+  }
+  
+  std::string hashWithSodium(const std::string &data) {
+    size_t HASH_SIZE = crypto_generichash_BYTES;
+    unsigned char hash[HASH_SIZE];
+    // hashing unkeyed data
+    if (crypto_generichash(
+            hash, HASH_SIZE,
+            reinterpret_cast<const unsigned char *>(data.c_str()), data.size(),
+            nullptr, 0) != 0) {
+      throw std::runtime_error("Failed to hash data");
+    }
+
+    // convert binary to hex
+    std::ostringstream oss;
+    for (size_t i = 0; i < HASH_SIZE; ++i) {
+      oss << std::hex << std::setw(2) << std::setfill('0') << (int)hash[i];
+    }
+    return oss.str();
+  }
+
+
+
+
+};
+} // namespace IronBlock
+
+#endif // BLOCK_H
