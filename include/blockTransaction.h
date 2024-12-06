cc6acea8d67e05266f109b2ed93667f68d1c6cd8
vineetpandey <vineetpandey1729@gmail.com>
2024-12-06 13:58:37 -0500

    removed the classes

--- include/blockTransaction.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by vineet on 12/6/24.
-//
-
-#ifndef BLOCKTRANSACTION_H
-#define BLOCKTRANSACTION_H
-
-#endif //BLOCKTRANSACTION_H
--- include/blockbuilder.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by vineet on 12/6/24.
-//
-
-#ifndef BLOCKBUILDER_H
-#define BLOCKBUILDER_H
-
-#endif //BLOCKBUILDER_H
